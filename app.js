a4d79d57bf21831d6b484a7322fb26281cff82b2
Erik Plyushko <eplyushko@gmail.com>
2017-07-26 18:24:07 -0700

    cache done

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function Photo(number) {
   Photo.all.push(this);
 }
 Photo.all = [];
-Photo.allNames = ['bag', 'banana', 'bathroom', 'boots','breakfast','bubblegum','chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun','usb','water-can','wine-glass'];
+Photo.allNames = ['bag', 'banana', 'bathroom', 'boots','breakfast','bubblegum','chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun','usb','water-can','wine-glass', 'unicorn'];
 
 for(var i = 0; i < Photo.allNames.length; i++){
   new Photo(Photo.allNames[i]);
@@ -26,19 +26,22 @@ Photo.imgConatner = document.getElementById('img_contaner');
 function randomNumber(){
   return Math.floor(Math.random() * Photo.all.length);
 }
+var numbers = [];
 function renderImg(){
-  var numbers = [];
   numbers[0] = randomNumber();
   numbers[1] = randomNumber();
   numbers[2] = randomNumber();
 
-  while(numbers[0] === numbers[1] || numbers[0] === numbers[2] || numbers[1] === numbers[2]){
+  while(numbers[0] === numbers[1]){
     console.log('Dupe Found!');
     numbers[1] = randomNumber();
+  }
+  while(numbers[2] === numbers[1] || numbers[2] === numbers[0]){
     numbers[2] = randomNumber();
   }
   for(var i = 0; i < numbers.lenght; i++){
     while (numbers[i] === lastShowen[0] || numbers[i] === lastShowen[1] || numbers[i] === lastShowen[2] || numbers[0] === numbers[1] || numbers[0] === numbers[2] || numbers[1] === numbers[2]) {
+      console.log('for while dupe');
       numbers[i] = randomNumber();
     }
   }
@@ -52,22 +55,31 @@ function renderImg(){
   Photo.imgElThree.alt = Photo.all[numbers[2]].name;
   Photo.all[numbers[2]].timesShown += 1;
   lastShowen = numbers;
+  // console.log(lastShowen);
 }
 
-function showList(){
-  var ulEl = document.getElementById('list');
-
-  for(var i = 0; i < Photo.all.length; i++){
-    var liEl = document.createElement('li');
-    liEl.textContent = Photo.all[i].name + ' was shown ' + Photo.all[i].timesShown + ' times, and clicked ' + Photo.all[i].timesClicked + ' times.';
-    ulEl.appendChild(liEl);
-  }
+function cache(){
+  localStorage.setItem('clicks', JSON.stringify(clicks));
+  localStorage.setItem('shown', JSON.stringify(shown));
 }
+function summonCache(){
+  clicks = JSON.parse(localStorage.clicks);
+  shown = JSON.parse(localStorage.shown);
+}
+
+// function showList(){
+//   var ulEl = document.getElementById('list');
+//
+//   for(var i = 0; i < Photo.all.length; i++){
+//     var liEl = document.createElement('li');
+//     liEl.textContent = Photo.all[i].name + ' was shown ' + Photo.all[i].timesShown + ' times, and clicked ' + Photo.all[i].timesClicked + ' times.';
+//     ulEl.appendChild(liEl);
+//   }
+// }
 function handleClick(e){
-  if(e.target.id === Photo.imgConatner){
-    alert('please select a photo');
-    return;
-  };
+  if(e.target.id === 'img_contaner'){
+    return alert('Please select a photo');
+  }
   Photo.totalClicks += 1;
   console.log(e.target.alt);
   for(var i = 0; i < Photo.all.length; i++){
@@ -75,17 +87,27 @@ function handleClick(e){
       Photo.all[i].timesClicked += 1;
     }
   }
-  if(Photo.totalClicks === 25){
+  if(Photo.totalClicks === 5){
     Photo.imgConatner.removeEventListener('click', handleClick);
-    showList();
+    // showList();
     tableUpdate();
+    cache();
+    console.log('chache in bank yo!');
     drawChart();
     return;
   }
   renderImg();
 }
 Photo.imgConatner.addEventListener('click', handleClick);
-
+//++++++++++++++++checking storage+++++++++
+if(localStorage.clicks){
+  summonCache();
+  console.log('Summoned Cache!');
+}else{
+  for(var i = 0; i < Photo.allNames.length; i++){
+    new Photo(Photo.allNames[i]);
+  }
+};
 renderImg();
 
 //update table data
@@ -122,6 +144,7 @@ var data = {
         '#641E16',
         '#16A085',
         '#D35400',
+        '#42f4f1'
       ],
       hoverBackgroundColor:[
         '#D0D3D4',
@@ -145,6 +168,7 @@ var data = {
         '#D0D3D4',
         '#D0D3D4',
         '#D0D3D4',
+        '#D0D3D4'
       ]
     }
   ]
